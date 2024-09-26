74b381146ae9ecd606dd07a47ea74cfab1013fa0
Vovalker <zanin-vladimir@mail.ru>
2024-09-26 05:54:00 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 54, 'HI', True
+print(immutable_var)
+#immutable_var[0] = 34
+mutable_list = [1,2,3,43, 'HI', False]
+print(mutable_list)
+mutable_list [0] = 777
+print(mutable_list)
